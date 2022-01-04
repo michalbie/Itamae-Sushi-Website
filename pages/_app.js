593bb86de2b5b72a86490cc5276dc0585d86033c
michalbie <userofficeit@gmail.com>
2022-01-04 22:31:47 +0100

    Optimize fonts

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,22 @@ import "../styles/Delivery.scss";
 import "../styles/Contact.scss";
 import "../styles/Footer.scss";
 import "animate.css/animate.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Courgette&family=Kalam:wght@400;700&family=Kaushan+Script&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
